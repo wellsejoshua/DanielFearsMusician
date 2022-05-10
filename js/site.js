2829d579e7e464bd3f6df9701292d048f9922027
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-05-09 22:00:11 -0500

    updating the mailing list form and functions

--- js/site.js
+++ js/site.js
@@ -8,3 +8,27 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+const emailForm = document.querySelector('.email-form')
+if (emailForm) {
+  emailForm.addEventListener('submit', e => {
+    e.preventDefault();
+    processForm(emailForm);
+  })
+}
+
+const processForm = form => {
+  const data = new FormData(form)
+  data.append('form-name', 'mailingList');
+  fetch('/', {
+    method: 'POST',
+    body: data,
+  })
+  .then(() => {
+    form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+  })
+  .catch(error => {
+    form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+  })
+}
